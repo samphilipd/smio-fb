7c7addfd604e3f187ee39a8952153ccaef9e771d
Sam Davies <seivadmas@gmail.com>
2015-05-28 16:46:02 -0300

    Can return an array of like deltas over time for a page

--- src/smio_fb/db/core.clj
+++ src/smio_fb/db/core.clj
@@ -9,3 +9,21 @@
    :password ""})
 
 (defqueries "sql/queries.sql" {:connection db-spec})
+
+(defn format-as-array
+  "Converts a results hash into an array suitable for injecting into a Flot chart"
+  [results]
+  (taoensso.timbre/info results)
+  (map
+    #(identity [(:timestamp %) (:n_likes %)])
+    results))
+
+(defn evolution-chart
+  "Queries the DB and returns a data structure suitable for injecting into the
+   evolution chart of the semant.io frontend"
+  [metric, page_id, start_time, end_time]
+  (taoensso.timbre/info (str start_time end_time))
+  (condp = metric
+    "likes" (format-as-array (get-page-deltas {:page_id page_id :start_time start_time :end_time end_time}))))
+
+;; change
--- src/smio_fb/routes/api.clj
+++ src/smio_fb/routes/api.clj
@@ -3,15 +3,46 @@
             [clojure.java.io :as io]
             [taoensso.timbre :as timbre]
             [clojure.data.json :as json]
-            [smio-fb.db.core :as db]))
+            [smio-fb.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v :refer [defvalidator]]
+            [clj-time.format :as f]
+            [clj-time.coerce :as c]))
+
+(def valid-metrics ["likes"])
+(def datetime-formatter (:date-time-no-ms f/formatters))
+(defvalidator metric-validator
+  {:default-message-format (str "%s must be one of " (clojure.string/join ", "valid-metrics))}
+  [metric]
+  (some #{metric} valid-metrics))
+
+(defn to-db-time
+  "Converts an ISO formatted time string into a SQL timestamp"
+  [iso-time-string]
+  (c/to-sql-time (f/parse datetime-formatter iso-time-string)))
+
+(defn validate
+  "Can we do something useful with the supplied parameters?"
+  [params]
+  (b/validate params
+    :page_id v/required
+    :metric metric-validator
+    :start_time [[v/required] [v/datetime datetime-formatter]]
+    :end_time [[v/required] [v/datetime datetime-formatter]]))
 
 (defn api-v1
   "Responds to requests to version one of the semant.io facebook REST API"
   [params]
-  (let [page_id (:page_id params)]
-    (let [response (db/get-page-deltas {:page_id (new BigDecimal page_id)})]
-      (timbre/info response)
-      response)))
+  ; (timbre/info (str params))
+  (let [{:keys [metric page_id start_time end_time]} params
+        errors (validate params)]
+    (if (seq? errors)
+      {:status 400 :body {:errors (first errors)}}
+      {:status 200 :body (db/evolution-chart
+        (str metric)
+        (bigdec page_id)
+        (to-db-time start_time)
+        (to-db-time end_time))})))
 
 (defroutes api-routes
   (GET "/" request (keys request))
